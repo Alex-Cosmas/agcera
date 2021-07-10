5f283b12f41f0c2fabb0a8dcb15c41bd0535e6a9
Alex Cosmas <cosmasalex365@gmail.com>
2021-07-10 23:27:11 +0300

    Featured Cards

--- components/Featured.js
+++ components/Featured.js
@@ -11,16 +11,24 @@ const products = [
     price: '70',
   },
   {
-    image: '../AGCERA.jpg',
-    title: 'Ag Cera',
+    image: '../agbroc_product.png',
+    title: 'Ag Broc',
     description:
       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? ',
 
     price: '70',
   },
   {
-    image: '../AGCERA.jpg',
-    title: 'Ag Cera',
+    image: '../AGBeauty-products.jpg',
+    title: 'Ag Beauty',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? ',
+
+    price: '70',
+  },
+  {
+    image: '../agcoffee_product.png',
+    title: 'Ag Coffee',
     description:
       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? ',
 
@@ -34,19 +42,18 @@ function FeaturedProduct() {
       <div className='flex flex-col items-center justify-center px-4 py-10 mx-auto md:px-24'>
         <h1 className='my-4 text-3xl'>Featured Products</h1>
 
-        <div className='grid gap-4 md:grid-cols-3'>
+        <div className='grid gap-4 md:grid-cols-4'>
           {products.map(function (featured, idx) {
             return (
-              <div key={idx} className='flex'>
+              <div key={idx} className='flex flex-col'>
+                <div className='p-4'>
+                  <div className='text-xl font-semibold text-center text-gray-500'>
+                    {featured.title}
+                  </div>
+                  {/* <div>{featured.description}</div> */}
+                </div>
                 <div className='overflow-hidden bg-white rounded-md shadow-xl'>
                   <img src={featured.image} alt='' className='w-full h-60' />
-
-                  <div className='p-4'>
-                    <div className='text-xl font-semibold text-center text-gray-500'>
-                      {featured.title}
-                    </div>
-                    {/* <div>{featured.description}</div> */}
-                  </div>
 
                   <div>
                     <button className='w-full py-4 text-center text-white bg-gray-900'>
